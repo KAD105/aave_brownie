fea9debd2bf636f72ef2cc37910650e74c395547
KAD105 <kazem_abdi@yahoo.com>
2022-05-11 06:28:09 +0200

    ERC20 interface added

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,6 +1,9 @@
 from brownie import network, config, interface
 from scripts.get_weth import get_weth
 from scripts.helpful_scripts import get_account
+from web3 import Web3
+
+amount = Web3.toWei(0.1, "ether")
 
 
 def main():
@@ -9,7 +12,21 @@ def main():
     if network.show_active() in ["mainnet-fork-dev"]:
         get_weth()
     lending_pool = get_lending_pool()
-    print(lending_pool)
+
+    # spender(the address who use our token) here is lending_pool,
+    # but we only need its address
+    approve_erc20(lending_pool.address, amount, erc20_address, account)
+
+    # deposit weth into aave, for this we use the deposit function from lendingPool  
+
+
+def approve_erc20(spender, amount, erc20_address, account):
+    print("Approving ERC20 token!")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("ERC20 token is approved to the spender!")
+    return tx
 
 
 def get_lending_pool():
